5d042a04b89307db00058d97377e629e8ef0743b
X <den2007fad@gmail.com>
2025-10-22 14:02:36 +0300

    feat(utils): Добавлена утилита для работы с файлами

---
+++ Client/src/utils/fileUtils.ts
@@ -0,0 +1,28 @@
+import { api } from "../api";
+import type { CourseDTO } from "../types/CouerseCard";
+import type { ProfileResponseDTO } from "../types/profile";
+
+export const getFileUrl = (fileUrl?: string): string | null => {
+    if(!fileUrl){
+        return null;
+    }
+
+    if(fileUrl.startsWith('http')){
+        return fileUrl;
+    }
+
+
+    if (fileUrl.startsWith('/')) {
+    const baseUrl = api || '';
+    return `${baseUrl}${fileUrl}`;
+  }
+  return fileUrl;
+};
+
+export const getAvatarUrl = (profile: ProfileResponseDTO): string | null => {
+    return getFileUrl(profile.avatarUrl);
+}
+
+export const getCoursePreviewUrl = (course: CourseDTO): string | null => {
+    return getFileUrl(course.previewUrl);
+}
