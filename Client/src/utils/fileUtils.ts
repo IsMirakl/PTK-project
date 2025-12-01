12f647321dd2aa4bab048df09add493f859c4e91
X <den2007fad@gmail.com>
2025-12-01 12:48:42 +0300

    fix: fix import type

--- Client/src/utils/fileUtils.ts
+++ Client/src/utils/fileUtils.ts
@@ -1,18 +1,17 @@
-import { api } from "../api";
-import type { CourseDTO } from "../types/CouerseCard";
-import type { ProfileResponseDTO } from "../types/profile";
+import { api } from '../api';
+import type { CourseDTO } from '../types/CourseCard';
+import type { ProfileResponseDTO } from '../types/profile';
 
 export const getFileUrl = (fileUrl?: string): string | null => {
-    if(!fileUrl){
-        return null;
-    }
+  if (!fileUrl) {
+    return null;
+  }
 
-    if(fileUrl.startsWith('http')){
-        return fileUrl;
-    }
+  if (fileUrl.startsWith('http')) {
+    return fileUrl;
+  }
 
-
-    if (fileUrl.startsWith('/')) {
+  if (fileUrl.startsWith('/')) {
     const baseUrl = api || '';
     return `${baseUrl}${fileUrl}`;
   }
@@ -20,9 +19,9 @@ export const getFileUrl = (fileUrl?: string): string | null => {
 };
 
 export const getAvatarUrl = (profile: ProfileResponseDTO): string | null => {
-    return getFileUrl(profile.avatarUrl);
-}
+  return getFileUrl(profile.avatarUrl);
+};
 
 export const getCoursePreviewUrl = (course: CourseDTO): string | null => {
-    return getFileUrl(course.previewUrl);
-}
+  return getFileUrl(course.previewUrl);
+};
